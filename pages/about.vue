8a1d71af64e8e1fee52058fd0f1274fbc05aa6c6
Boahmu <boahmu@proton.me>
2025-03-23 19:57:47 +0100

    Init project

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to the aboutpage</h1>
+    <p>
+      Lorem impsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+    </p>
+  </div>
+</template>
