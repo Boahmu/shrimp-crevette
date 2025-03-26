6241710b07077937d7e8f92cc88d59ec91ff2f02
Boahmu <boahmu@proton.me>
2025-03-26 20:58:49 +0100

    try to setup darkmode

--- app.vue
+++ app.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
+  <div class="bg-neutral-400 dark:bg-old-neutral-950 relative transition-colors duration-200">
+    test
     <NuxtRouteAnnouncer />
     <NuxtPage />
+    <button @click="here()"> ICI</button>
   </div>
 </template>
+
+<script setup>
+function here() {
+  console.log(darkMode.value)
+  if(darkMode.value){
+    document.documentElement.classList.add('dark');
+    darkMode.value = false;
+  } else {
+    document.documentElement.classList.remove('dark');
+    darkMode.value = true;
+  }
+  console.log('ICI')
+}
+
+const darkMode = ref(false);
+</script>
 
 <style>
 div {
