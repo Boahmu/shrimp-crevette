f1979add4ea5e0a755268c42ad3c7f6e5f93ed94
Boahmu <boahmu@proton.me>
2025-03-26 21:34:27 +0100

    setup darkmode with color mode from nuxt/ui

--- app.vue
+++ app.vue
@@ -1,26 +1,22 @@
 <template>
-  <div class="bg-neutral-400 dark:bg-old-neutral-950 relative transition-colors duration-200">
-    test
+  <div class="bg-neutral-400 dark:bg-gray-800 transition-colors duration-500">
     <NuxtRouteAnnouncer />
-    <NuxtPage />
-    <button @click="here()"> ICI</button>
+    <NuxtPage class="text-cyan-950 dark:text-white" />
+    <button @click="isDark = !isDark"> ICI</button>
   </div>
 </template>
 
 <script setup>
-function here() {
-  console.log(darkMode.value)
-  if(darkMode.value){
-    document.documentElement.classList.add('dark');
-    darkMode.value = false;
-  } else {
-    document.documentElement.classList.remove('dark');
-    darkMode.value = true;
-  }
-  console.log('ICI')
-}
+const colorMode = useColorMode()
 
-const darkMode = ref(false);
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
 </script>
 
 <style>
