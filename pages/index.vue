f3a15ee2d82c52aeb3659862b339457ab521e08f
Boahmu <boahmu@proton.me>
2025-03-26 19:48:39 +0100

    Finished tailwind config, it's working

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
+    <h1 class="text-blue-300">Welcome to the homepage</h1>
     <p>
       Lorem impsum dolor sit amet, consectetur adipiscing elit. Nulla nec
     </p>
