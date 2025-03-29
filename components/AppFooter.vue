0d6d9bcdc5418fa1bc5b28365041992f09b62554
Boahmu <boahmu@proton.me>
2025-03-29 21:18:28 +0100

    nav bar md, soon done

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,24 +2,46 @@
 </script>
 
 <template>
-  <footer class="bg-zinc-900/90 bottom-0 backdrop-blur-xs">
-    <div class="flex justify-between items-center p-4 text-zinc-100">
-      <nav class="flex space-x-4">
-        <NuxtLink class="hover:text-green-500" to="/">Accueil</NuxtLink>
-        <NuxtLink class="hover:text-green-500" to="/guide">Guide</NuxtLink>
-        <NuxtLink class="hover:text-green-500" to="/blog">Blog</NuxtLink>
-        <NuxtLink class="hover:text-green-500" to="/about">A propos</NuxtLink>
-      </nav>
-      <div>
-        <h2 class="text-lg font-bold">Contact Me</h2>
-        <form class="flex space-x-2">
-          <input type="text" placeholder="Your Name" class="p-2 rounded bg-zinc-800 text-zinc-100">
-          <input type="email" placeholder="Your Email" class="p-2 rounded bg-zinc-800 text-zinc-100">
-          <textarea placeholder="Your Message" class="p-2 rounded bg-zinc-800 text-zinc-100"/>
-          <button type="submit" class="p-2 rounded bg-green-500 text-zinc-100 hover:bg-green-600">Send</button>
+  <footer class="bg-zinc-900/90 bottom-0 backdrop-blur-xs border border-t-green-500">
+    <div
+        class="flex flex-col  gap-6 p-4 text-zinc-100 md:flex-row md:flex-wrap justify-center">
+
+      <div class="mt-4 text-center min-w-90">
+        <h2 class="text-lg font-bold">Retrouve-nous sur les réseaux sociaux !</h2>
+        <div class="flex justify-center gap-6 mt-3">
+          <Icon name="logos:facebook" size="36"/>
+          <Icon name="skill-icons:instagram" size="36"/>
+          <Icon name="skill-icons:discord" size="36"/>
+        </div>
+
+      </div>
+
+
+      <div class="mt-4 text-center min-w-90">
+        <h2 class="text-lg font-bold">Navigation rapide</h2>
+        <nav class="flex justify-center gap-6">
+          <NuxtLink class="hover:text-green-500" to="/">Accueil</NuxtLink>
+          <NuxtLink class="hover:text-green-500" to="/guide">Guide</NuxtLink>
+          <NuxtLink class="hover:text-green-500" to="/blog">Blog</NuxtLink>
+          <NuxtLink class="hover:text-green-500" to="/about">A propos</NuxtLink>
+        </nav>
+      </div>
+
+      <div class="mt-4 text-center min-w-90">
+        <h2 class="text-lg font-bold">Inscris-toi à notre newsletter !</h2>
+        <form class="flex justify-center mt-3">
+          <input type="text" placeholder="Email" class="p-2 rounded bg-zinc-800 text-zinc-100 min-w-60">
+          <button type="submit" class="p-2 rounded bg-green-500 text-zinc-100 hover:bg-green-600">Ok</button>
         </form>
       </div>
+
+      <div class="mt-4 text-center md:w-full">
+        <h2 class="text-lg font-bold">Contact nous !</h2>
+        <a href="mailto:contact@shrimp-crevette.com" class="text-green-500 hover:text-green-600">contact@shrimp-crevette.com</a>
+      </div>
     </div>
+
+
     <div class="text-center p-4 text-zinc-100">
       &copy; Shrimp Crevette - Made with
       <Icon name="mdi:heart" class="text-green-500"/>
